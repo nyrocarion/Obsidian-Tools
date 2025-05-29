9f9ed04f1ad79a097633e92f0941c63810bd0045
nyronik <106926816+nyrocarion@users.noreply.github.com>
2025-05-29 09:57:00 +0200

    feat: order in unit script
    a small programm that produces a markdown table with latex inside to show the calculation of the order of an element in a unit

---
+++ primzahlen/ordnung_in_einheitengruppe.py
@@ -0,0 +1,20 @@
+def order_in_unit(n,a):
+    # Z_n
+    # a^i
+    n = int(n)
+    a = int(a)
+    unit = [elem for elem in range(1,11)]
+    res = 0
+    print(f"|$i$|${a}^i \mod {n}$|")
+    print("|-|-|")
+    counter = 1
+    while res != 1 and counter <= len(unit):
+        res = a**counter % n
+        print(f"|${counter}$|${a}^{counter}\equiv{res}\mod {n}$|")
+        counter += 1
+    print("-> d.h. $\\text{ord}("+str(a)+")="+str(counter-1)+"$ in $\mathbb Z_{"+str(n)+"}^*$")
+
+if __name__ == "__main__":
+    n = input("Z_n^* with n=           ")
+    a = input("Berechne Ordnung von a= ")
+    order_in_unit(n,a)
